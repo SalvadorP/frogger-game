aaeb590cb6e2788480dc58d7011cbdf173b3beea
Salvador P <salvadorperezd@gmail.com>
2017-09-15 12:21:33 +0200

    Added all the required libraries, avoiding node_modules
    Check a way to automatize this, with webpack or gulp

--- js/app.js
+++ js/app.js
@@ -29,3 +29,14 @@ $('#up').hammer().bind("panup tap press", {action: 'up'}, actionHandler);
 $('#down').hammer().bind("pandown tap press", {action: 'down'}, actionHandler);
 $('#left').hammer().bind("panleft tap press", {action: 'left'}, actionHandler);
 $('#right').hammer().bind("panright tap press", {action: 'right'}, actionHandler);
+
+// $(document).ready(function(){
+//     var board = $('#board canvas');
+//     $(board).hammer().bind("tap", canvasTapHandler);
+//     console.log();
+
+// });
+
+// function canvasTapHandler(event) {
+//     console.log(event);
+// }
