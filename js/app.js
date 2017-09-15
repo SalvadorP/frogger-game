41d4a0618b9ec025ecf3ca4cead320c4a3d5455a
Salvador P <salvadorperezd@gmail.com>
2017-09-15 12:02:05 +0200

    Refactorized the tap event

--- js/app.js
+++ js/app.js
@@ -25,52 +25,7 @@ document.addEventListener('keyup', function(e) {
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
-// var up = $('#up');
-// var left = $('#left');
-// var right = $('#right');
-// var down = $('#down');
-
-// var mcUp = new Hammer(up);
-// var mcDown = new Hammer(down);
-
-// // listen to events...
-// mcUp.on("panleft panright tap press", function(ev) {
-//     // myElement.textContent = ev.type +" gesture detected.";
-//     console.log("up!");
-// });
-// mcDown.on("panleft panright tap press", function(ev) {
-//     // myElement.textContent = ev.type +" gesture detected.";
-//     console.log("down!");
-// });
-
-// IDEA: Make it DRY!!
-$('#up').hammer().bind("panup tap press", upHandler);
-$('#down').hammer().bind("pandown tap press", downHandler);
-$('#left').hammer().bind("panleft tap press", leftHandler);
-$('#right').hammer().bind("panright tap press", rightHandler);
-
-function upHandler() {
-    player.handleInput('up');
-}
-
-function downHandler() {
-    player.handleInput('down');
-}
-function leftHandler() {
-    player.handleInput('left');
-}
-
-function rightHandler() {
-    player.handleInput('right');
-}
-
-
-// $('#up').hammer().bind("panup tap press", actionHandler('up'));
-// $('#down').hammer().bind("pandown tap press", actionHandler('down'));
-// $('#left').hammer().bind("panleft tap press", actionHandler('left'));
-// $('#right').hammer().bind("panright tap press", actionHandler('right'));
-
-// function actionHandler(action) {
-//     player.handleInput(action);
-// }
-
+$('#up').hammer().bind("panup tap press", {action: 'up'}, actionHandler);
+$('#down').hammer().bind("pandown tap press", {action: 'down'}, actionHandler);
+$('#left').hammer().bind("panleft tap press", {action: 'left'}, actionHandler);
+$('#right').hammer().bind("panright tap press", {action: 'right'}, actionHandler);
