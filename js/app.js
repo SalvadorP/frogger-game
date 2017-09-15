81e097dafae3497b7c92a10910782d763d63ea85
Salvador P <salvadorperezd@gmail.com>
2017-09-15 11:52:35 +0200

    Added hammerjs library for touch input
    Now the buttons should respond to the touch.
    Added package.json to handle the required libraries.

--- js/app.js
+++ js/app.js
@@ -24,3 +24,53 @@ document.addEventListener('keyup', function(e) {
     };
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+// var up = $('#up');
+// var left = $('#left');
+// var right = $('#right');
+// var down = $('#down');
+
+// var mcUp = new Hammer(up);
+// var mcDown = new Hammer(down);
+
+// // listen to events...
+// mcUp.on("panleft panright tap press", function(ev) {
+//     // myElement.textContent = ev.type +" gesture detected.";
+//     console.log("up!");
+// });
+// mcDown.on("panleft panright tap press", function(ev) {
+//     // myElement.textContent = ev.type +" gesture detected.";
+//     console.log("down!");
+// });
+
+// IDEA: Make it DRY!!
+$('#up').hammer().bind("panup tap press", upHandler);
+$('#down').hammer().bind("pandown tap press", downHandler);
+$('#left').hammer().bind("panleft tap press", leftHandler);
+$('#right').hammer().bind("panright tap press", rightHandler);
+
+function upHandler() {
+    player.handleInput('up');
+}
+
+function downHandler() {
+    player.handleInput('down');
+}
+function leftHandler() {
+    player.handleInput('left');
+}
+
+function rightHandler() {
+    player.handleInput('right');
+}
+
+
+// $('#up').hammer().bind("panup tap press", actionHandler('up'));
+// $('#down').hammer().bind("pandown tap press", actionHandler('down'));
+// $('#left').hammer().bind("panleft tap press", actionHandler('left'));
+// $('#right').hammer().bind("panright tap press", actionHandler('right'));
+
+// function actionHandler(action) {
+//     player.handleInput(action);
+// }
+
